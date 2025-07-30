d2bbd8b8a3bbca539bd289f1bda8c6d6cebad08e
Abhishekkaddipudi <abhishekkaddipudi123@gmail.com>
2025-07-30 13:35:31 +0530

    the-amber-sword (v1.0)

--- temp/url_extractor.py
+++ temp/url_extractor.py
@@ -7,7 +7,7 @@ import json
 import re
 from git_cmsg import update_novel_title_version
 
-def build_url(url="https://readnovelfull.com/the-book-eating-magician-v1.html#tab-chapters-title",):
+def build_url(url="https://novelbin.com/b/the-amber-sword#tab-chapters-title",):
     update_novel_title_version(url)
     if 2 > 1:
         options = ChromeOptions()
