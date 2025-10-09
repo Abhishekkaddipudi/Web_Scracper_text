201455af2f9c003ef929521481c1660eff188680
Abhishekkaddipudi <abhishekkaddipudi123@gmail.com>
2025-10-09 10:39:17 +0530

    you-cultivate-i-farm (v1.0)

--- temp/url_extractor.py
+++ temp/url_extractor.py
@@ -7,7 +7,7 @@ import json
 import re
 from git_cmsg import update_novel_title_version
 
-def build_url(url="https://novelbin.org/novelbin/i-shall-seal-the-heavens-14570018#tab-chapters-title",):
+def build_url(url="https://readnovelfull.com/absolute-resonance.html#tab-chapters-title",):
     update_novel_title_version(url)
     if 2 > 1:
         options = ChromeOptions()
