d2d1094c3094261e96fcf608e7945f4799370ff4
Abhishekkaddipudi <abhishekkaddipudi123@gmail.com>
2025-10-15 08:32:06 +0530

    i-cultivate-passively (v1.0)

--- temp/url_extractor.py
+++ temp/url_extractor.py
@@ -7,7 +7,7 @@ import json
 import re
 from git_cmsg import update_novel_title_version
 
-def build_url(url="https://readnovelfull.com/emperors-domination.html#tab-chapters-title",):
+def build_url(url="https://novelbin.com/b/i-cultivate-passively#tab-chapters-title",):
     update_novel_title_version(url)
     if 2 > 1:
         options = ChromeOptions()
